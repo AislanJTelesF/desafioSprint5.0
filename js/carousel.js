33e3f8c44aba09a1750546ce5d1cd67c01b5e1b5
AislanJTelesF <aislanjesus2019@gmail.com>
2025-05-28 09:20:09 -0300

    finalizando carrossel

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,3 @@
-
-
-//carousel
-
-//class Carousel
 class Carousel {
 
     constructor(imagem, texto, link) {
@@ -11,29 +6,9 @@ class Carousel {
         this.link = link;
     }
       
-    // static Start(arr){
-    //     if(arr){
-
-    //         if(arr.length > 0){
-    //             Carousel._sequence = 0;
-    //             Carousel._size = arr.length;
-    //             Carousel.Next(); //start
-    //             Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-    //         }
-            
-    //     } else {
-    //         throw "Method Start need a Array Variable.";
-    //     }
-    // }
-
-    // static Next(){
-        
-    // }
 };
 
-//Array storage class
 const carouselArr = [];
-
 
 let currentIndex = 0;
 
@@ -56,5 +31,5 @@ function slideAnterior() {
 
 window.onload = () => {
     mostrarSlide(currentIndex);
-    setInterval(nextSlide, 2000); // mudar a cada 2 segundos
+    setInterval(slideProximo, 3000); // mudar a cada 3 segundos
 };
